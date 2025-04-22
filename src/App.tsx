4fa71c3974b6ccd1a0b0c8c222725ab4195e7201
Mohammed Ismail <mohammedismail523261@gmail.com>
2025-04-22 20:03:25 +0530

    Initial commit - detached from old repo

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { DataProvider } from "./context/DataContext";
+import MainLayout from "./components/layout/MainLayout";
+
+import Dashboard from "./pages/Dashboard";
+import AddExpense from "./pages/AddExpense";
+import Expenses from "./pages/Expenses";
+import Wallet from "./pages/Wallet";
+import Goals from "./pages/Goals";
+import Notifications from "./pages/Notifications";
+import Settings from "./pages/Settings";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <DataProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="add-expense" element={<AddExpense />} />
+              <Route path="expenses" element={<Expenses />} />
+              <Route path="wallet" element={<Wallet />} />
+              <Route path="goals" element={<Goals />} />
+              <Route path="notifications" element={<Notifications />} />
+              <Route path="settings" element={<Settings />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </DataProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
